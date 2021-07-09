88594ec6c4ef63d76aecc78ada6b2e0777c3eb66
Dan Miller <dsmiller95@gmail.com>
2021-07-09 15:07:11 -0500

    add a scriptable object health component, with a healthy component. make the enemies damage the player's health when they traverse across the whole field

---
+++ Assets/scripts/scriptableObjects/FloatTextDisplay.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatTextDisplay: MonoBehaviour
+{
+    public FloatVariable dataSource;
+    public Text textComponent;
+    public string textFormat = "# shmeckles";
+
+    private void Awake()
+    {
+        dataSource.onValueChanged += OnDataChanged;
+    }
+
+    private void OnDestroy()
+    {
+        // must remove the listener here. if not, then the next change will try to 
+        //  invoke a method on this object after it has been destroyed
+        //  this displeases the Unity
+        dataSource.onValueChanged -= OnDataChanged;
+    }
+
+    private void OnDataChanged()
+    {
+        this.textComponent.text = textFormat.Replace("#", $"{dataSource.Value:F1}");
+    }
+}
