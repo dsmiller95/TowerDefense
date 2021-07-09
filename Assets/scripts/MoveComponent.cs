826896b57b8c01fdc8e4ef899b35f2c360005892
Dan Miller <dsmiller95@gmail.com>
2021-07-09 13:37:28 -0500

    add bullet and modular components which make it easy to compose bullet type things

--- Assets/scripts/MoveComponent.cs
+++ Assets/scripts/MoveComponent.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class EnemyComponent : MonoBehaviour
+public class MoveComponent : MonoBehaviour
 {
+    public float movementSpeed = 1f;
     private void FixedUpdate()
     {
         // moving the component must happen in FixedUpdate, so the physics system has time to update
         //  otherwise, the collider position will no longer be in sync with the actual transform position
-        transform.position += Vector3.forward * Time.fixedDeltaTime;
+        transform.position += (transform.forward * Time.fixedDeltaTime * movementSpeed);
     }
 }
