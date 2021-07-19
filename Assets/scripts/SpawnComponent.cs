194dc6247d321064dee5940e67f4c86e6de1fb0e
Dan Miller <dsmiller95@gmail.com>
2021-07-19 13:41:43 -0500

    add scaling via an animation curve to the spawn component

--- Assets/scripts/SpawnComponent.cs
+++ Assets/scripts/SpawnComponent.cs
@@ -8,7 +8,12 @@ public class SpawnComponent : MonoBehaviour
     public GameObject spawnedPrefab;
     public Vector3 spawnVolume = Vector3.one;
 
-    public float secondsPerSpawn;
+    /// <summary>
+    /// frequency in hertz of spawns 
+    /// </summary>
+    public AnimationCurve spawnFrequency;
+
+    private float secondsPerSpawn => 1f / spawnFrequency.Evaluate(Time.time);
     private float lastSpawnTime = 0;
     private void Update()
     {
