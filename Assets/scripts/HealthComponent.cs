e3b420f37a08e647deaa7d1ccbdf6fcc93937329
Dan Miller <dsmiller95@gmail.com>
2021-07-20 12:39:42 -0500

    add bullet prefab, and firing control components

---
+++ Assets/scripts/HealthComponent.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+public class HealthComponent : MonoBehaviour
+{
+    public float health;
+
+    public void DamageHealth(float damage)
+    {
+        this.health -= damage;
+
+        OnHealthChanged();
+    }
+
+    private void OnHealthChanged()
+    {
+        if(health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
